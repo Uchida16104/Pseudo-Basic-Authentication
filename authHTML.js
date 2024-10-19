95243b6f0d9d2f23f96ee63d1b97554d9b4dec2f
Hirotoshi Uchida <uho02741358@gmail.com>
2024-10-19 15:44:12 +0900

    Create authHTML.js

---
+++ authHTML.js
@@ -0,0 +1,30 @@
+       function authenticate() {
+           const username = prompt('Please enter user name:');
+           const password = prompt('Please enter password:');
+           const users = JSON.parse(localStorage.getItem('users')) || [];
+
+           const user = users.find(user => user.username === username && user.password === password);
+
+           if (user) {
+               alert('Authentication succeeded!');
+               window.location.href = 'welcome.html';
+           } else {
+               alert('Authentication failed.');
+               window.location.href = 'error.html';
+           }
+       }
+
+       function checkAuthStatus() {
+           const isAuthenticated = localStorage.getItem('isAuthenticated');
+           if (!isAuthenticated) {
+               authenticate();
+           }
+       }
+
+      function markAuthenticated() {
+           localStorage.setItem('isAuthenticated', 'true');
+       }
+
+       window.onload = () => {
+           checkAuthStatus();
+       };
